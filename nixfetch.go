f62a2d36d686014afa80878c4ec8cd5d11f47ca6
Nick <nickhardbarger13@gmail.com>
2025-08-14 07:44:40 -0400

    greatly simplified updated()

--- nixfetch.go
+++ nixfetch.go
@@ -5,10 +5,10 @@ import (
 	"log"
 	"math"
 	"os"
+	"os/exec"
 	"os/user"
 	"strconv"
 	"strings"
-	"time"
 )
 
 const (
@@ -34,7 +34,7 @@ const (
   // \\  \\`
 )
 
-func get_userhost() string {
+func userhost() string {
 	user, err := user.Current()
 	if err != nil {
 		log.Fatal(err)
@@ -46,7 +46,7 @@ func get_userhost() string {
 	return user.Username + "@" + host
 }
 
-func get_model() string {
+func model() string {
 	model_name, err := os.ReadFile("/sys/devices/virtual/dmi/id/product_version")
 	if err != nil {
 		log.Fatal(err)
@@ -55,7 +55,7 @@ func get_model() string {
 	return strings.Trim(string(model_name), "\n")
 }
 
-func get_flake() string {
+func flake() string {
 	flake_version, err := os.ReadFile("/etc/os-release")
 	if err != nil {
 		log.Fatal(err)
@@ -63,7 +63,7 @@ func get_flake() string {
 	return strings.Split(string(flake_version), "\"")[21]
 }
 
-func get_kernel() string {
+func kernel() string {
 	kernel_version, err := os.ReadFile("/proc/version")
 	if err != nil {
 		log.Fatal(err)
@@ -71,7 +71,7 @@ func get_kernel() string {
 	return strings.Split(string(kernel_version), " ")[2]
 }
 
-func get_uptime() string {
+func uptime() string {
 	uptime, err := os.ReadFile("/proc/uptime")
 	if err != nil {
 		log.Fatal(err)
@@ -99,28 +99,22 @@ func get_uptime() string {
 }
 
 func updated() string {
-	const six_months int64 = 15778463
-	// NOTE: date must be updated manually
-	start_time := time.Date(2025, 8, 1,
-		0, 0, 0, 0, time.UTC).Unix()
-	current_time := time.Now().Unix()
-	time_passage := current_time - start_time
-	days_passed := time_passage / 86400
-	if time_passage > six_months {
-		return red + strconv.Itoa(int(days_passed)) + " days ago!!"
-	} else {
-		return strconv.Itoa(int(days_passed)) + " days ago"
+	cmd := exec.Command("date", "+%Y-%m-%d", "-r", "/home/nh/nix/flake.lock")
+	out, err := cmd.Output()
+	if err != nil {
+		log.Fatal(err)
 	}
+	return strings.Trim(string(out), "\n")
 }
 
 func main() {
 	fmt.Print(logo + "\033[6A" + reset_cursor)
-	fmt.Println(green + get_userhost() + white)
+	fmt.Println(green + userhost() + white)
 	fmt.Println(reset_cursor + "┌──────────────────────────────┐")
-	fmt.Println(reset_cursor + red + " model " + white + get_model())
-	fmt.Println(reset_cursor + blue + " flake " + white + get_flake())
-	fmt.Println(reset_cursor + yellow + " kernel " + white + get_kernel())
-	fmt.Println(reset_cursor + cyan + " uptime " + white + get_uptime())
+	fmt.Println(reset_cursor + red + " model " + white + model())
+	fmt.Println(reset_cursor + blue + " flake " + white + flake())
+	fmt.Println(reset_cursor + yellow + " kernel " + white + kernel())
+	fmt.Println(reset_cursor + cyan + " uptime " + white + uptime())
 	fmt.Println(reset_cursor + purple + " updated " + white + updated())
 	fmt.Println(reset_cursor + "└──────────────────────────────┘")
 }
